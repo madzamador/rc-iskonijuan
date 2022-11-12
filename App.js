123485f0007e9de66ad15f052f367287c0838471
RamilSC <ramil.cobillajr@tup.edu.ph>
2022-11-12 23:34:02 +0800

    Added custom theme and fonts

--- App.js
+++ App.js
@@ -5,12 +5,24 @@ import {
   Text,
 } from "react-native-paper";
 
-import { StatusBar } from "expo-status-bar";
-import { View, StyleSheet } from "react-native";
+import { useFonts } from "expo-font";
+import { SafeAreaView } from "react-native";
+
+// Custom font integration of React Native Expo and React Native Paper
+
+const customThemeFonts = Object.fromEntries(
+  Object.entries(DefaultTheme.fonts).map(([variantName, variantProperties]) => [
+    variantName,
+    { ...variantProperties, fontFamily: "Inter-Black" },
+  ])
+);
+
+// Custom Theme Configuration for App
 
 const theme = {
   ...DefaultTheme,
   version: 3,
+  fonts: customThemeFonts,
   colors: {
     ...DefaultTheme.colors,
     primary: "#F55A5A",
@@ -21,23 +33,37 @@ const theme = {
 };
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Inter-Black": require("./assets/fonts/Inter-Black.otf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <PaperProvider theme={theme}>
-      <View style={styles.container}>
-        <Text variant='bodyMedium'>
-          Open up App.js to start working on your app!
-        </Text>
-        <StatusBar style='auto' />
-      </View>
+      <SafeAreaView>
+        <Text variant='displayLarge'>Display Large</Text>
+        <Text variant='displayMedium'>Display Medium</Text>
+        <Text variant='displaySmall'>Display small</Text>
+
+        <Text variant='headlineLarge'>Headline Large</Text>
+        <Text variant='headlineMedium'>Headline Medium</Text>
+        <Text variant='headlineSmall'>Headline Small</Text>
+
+        <Text variant='titleLarge'>Title Large</Text>
+        <Text variant='titleMedium'>Title Medium</Text>
+        <Text variant='titleSmall'>Title Small</Text>
+
+        <Text variant='bodyLarge'>Body Large</Text>
+        <Text variant='bodyMedium'>Body Medium</Text>
+        <Text variant='bodySmall'>Body Small</Text>
+
+        <Text variant='labelLarge'>Label Large</Text>
+        <Text variant='labelMedium'>Label Medium</Text>
+        <Text variant='labelSmall'>Label Small</Text>
+      </SafeAreaView>
     </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
