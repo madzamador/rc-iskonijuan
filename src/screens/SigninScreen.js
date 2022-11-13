a4ddca6ee49311e7d10f1264320b39af758f868f
RamilSC <ramil.cobillajr@tup.edu.ph>
2022-11-14 03:10:39 +0800

    Added SignUp Event Handlers

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,11 +1,45 @@
-import { StyleSheet, View } from "react-native";
-import React from "react";
-import { Text } from "react-native-paper";
+import React, { useState } from "react";
+import { StyleSheet, View, Image } from "react-native";
+import { Button, Text, TextInput } from "react-native-paper";
 
 export default function SigninScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text variant='bodyLarge'>HomeScreen</Text>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../../assets/images/logo-nobg.png")}
+          style={styles.imageIcon}
+        />
+      </View>
+      <View style={styles.signinContainer}>
+        <Text style={styles.signinText}>Sign In</Text>
+        <TextInput
+          mode='outlined'
+          label='Email address'
+          value={email}
+          onChangeText={(email) => setEmail(email)}
+          style={styles.textInput}
+        />
+        <TextInput
+          mode='outlined'
+          label='Password'
+          value={password}
+          secureTextEntry={!showPassword}
+          right={
+            <TextInput.Icon
+              icon={showPassword ? "eye" : "eye-off"}
+              onPress={() => setShowPassword(!showPassword)}
+            />
+          }
+          onChangeText={(password) => setPassword(password)}
+          forceTextInputFocus={false}
+          style={[styles.textInput, { marginTop: 15 }]}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +47,36 @@ export default function SigninScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#FDFCFB",
     alignItems: "center",
-    justifyContent: "center",
+    padding: 40,
+  },
+  imageIcon: {
+    width: 263,
+    height: 114,
+    resizeMode: "contain",
+  },
+  imageContainer: {
+    marginTop: 40,
+
+    alignItems: "center",
+  },
+  signinContainer: {
+    marginTop: 24,
+    borderBottomStartRadius: 0,
+    borderBottomEndRadius: 0,
+    width: "100%",
+    height: 200,
+  },
+  signinText: {
+    color: "#2B283A",
+    fontSize: 35,
+    fontWeight: "700",
+    marginTop: 24,
+    paddingBottom: 24,
+  },
+  textInput: {
+    fontSize: 18,
+    fontWeight: "600",
   },
 });
